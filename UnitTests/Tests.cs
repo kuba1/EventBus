bea9adbc34b82412a7e5fef52c9257c540377f40
Jakub Gołębiewski <kubusg@gmail.com>
2025-03-29 08:27:04 +0100

    Add first unit test.
    It's not particularly useful, added just to make `dotnet test` pass.

--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -1,1 +1,20 @@
 ﻿namespace Jgss.EventBus.UnitTests;
+
+record TestEvent : IEvent
+{
+    public string TestProperty { get; set; } = string.Empty;
+}
+
+public class EventBusTests
+{
+    [Fact(DisplayName = "Given event class when it is instantiated then instantiation succeeds")]
+    public void Given_event_class_when_it_is_instantiated_then_instantiation_succeeds()
+    {
+        var eventInstance = new TestEvent
+        {
+            TestProperty = "TestValue"
+        };
+
+        eventInstance.TestProperty.Should().Be("TestValue");
+    }
+}
